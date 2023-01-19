35f85a9a3402ed413f821a2852e7fd0aa6ff67a8
Diego <diego.lucassxavier@gmail.com>
2023-01-19 09:05:47 -0300

    Fixed sounds URL and added cross-browser compatibility to focus-timer-darkmode

--- level-05/focus-timer-darkmode/js/sounds.js
+++ level-05/focus-timer-darkmode/js/sounds.js
@@ -1,10 +1,10 @@
 export default function Sound() {
 	const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
   const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
-	const forest = new Audio('/assets/forest.wav')
-	const rain = new Audio("/assets/rain.wav")
-	const coffee = new Audio("/assets/coffee-shop.wav")
-	const fire = new Audio("/assets/fireplace.wav")
+	const forest = new Audio('./assets/forest.wav')
+	const rain = new Audio("./assets/rain.wav")
+	const coffee = new Audio("./assets/coffee-shop.wav")
+	const fire = new Audio("./assets/fireplace.wav")
 
 	forest.loop = true
 	rain.loop = true
